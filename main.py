9ecce1e226652609b742b94cb0b999c71ba885dc
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-12-03 10:32:24 +0300

    added hotkeys to shapes

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ class Window(QMainWindow):
 
         save_action = QAction('Save', self)
         save_action.setIcon(QIcon('icons/save_as_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        save_action.setShortcut('Ctrl+S')
         save_action.triggered.connect(self.canvas.saveImage)
         file_menu.addAction(save_action)
 
@@ -93,41 +94,49 @@ class Window(QMainWindow):
 
         brush_action = QAction('Brush', self)
         brush_action.setIcon(QIcon('icons/brush_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        brush_action.setShortcut('F1')
         brush_action.triggered.connect(self.canvas.setBrush)
         tools_menu.addAction(brush_action)
 
         line_action = QAction('Line', self)
         line_action.setIcon(QIcon('icons/diagonal_line_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        line_action.setShortcut('F2')
         line_action.triggered.connect(self.canvas.setLine)
         tools_menu.addAction(line_action)
 
         circle_action = QAction('Circle', self)
         circle_action.setIcon(QIcon('icons/animation_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        circle_action.setShortcut('F3')
         circle_action.triggered.connect(self.canvas.setCircle)
         tools_menu.addAction(circle_action)
 
         triangle_action = QAction('Triangle', self)
         triangle_action.setIcon(QIcon('icons/change_history_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        triangle_action.setShortcut('F4')
         triangle_action.triggered.connect(self.canvas.setTriangle)
         tools_menu.addAction(triangle_action)
 
         square_action = QAction('Square', self)
         square_action.setIcon(QIcon('icons/check_box_outline_blank_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        square_action.setShortcut('F5')
         square_action.triggered.connect(self.canvas.setSquare)
         tools_menu.addAction(square_action)
 
         star_action = QAction('Star', self)
         star_action.setIcon(QIcon('icons/star_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        star_action.setShortcut('F6')
         star_action.triggered.connect(self.canvas.setStar)
         tools_menu.addAction(star_action)
 
         arrow_action = QAction('Arrow', self)
         arrow_action.setIcon(QIcon('icons/arrow_forward_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        arrow_action.setShortcut('F7')
         arrow_action.triggered.connect(self.canvas.setArrow)
         tools_menu.addAction(arrow_action)
 
         text_action = QAction('Text', self)
         text_action.setIcon(QIcon('icons/text_fields_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg'))
+        text_action.setShortcut('F8')
         text_action.triggered.connect(self.canvas.setText)
         tools_menu.addAction(text_action)
 
